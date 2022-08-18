728b1bd432254d6a96a40347129a8dfe10a21347
Andrew <aivlev8@gmail.com>
2022-08-18 17:44:17 +0300

    Added 'Scroll to Top' button

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ let nextHref  = (item, index, array) => {
 	return index;
 };
 
-const prods=[
+const prods = [
 	`images/ck_dieticheskay_dobavka_lecitin.pdf`,
 	`images/ck_50_rastvor_etiloviy_spirt.pdf`,
 	`images/ck_50_rastvor_izopropiloviy_spirt.pdf`,
@@ -22,7 +22,7 @@ const prods=[
 	`images/ck_lecitin.pdf`,
 	``
 ];
-const certs=[
+const certs = [
 	`images/Kosher_certificate_2023.jpg`,
 	`images/ISO_22000_2018_2021.jpg`,
 	`images/tech_umovy1str.jpg`,
@@ -30,22 +30,22 @@ const certs=[
 	`images/tech_umovy1str_oliya.jpg`
 ];
 
-let prodIndex=0,
-	certsIndex=0;
+let prodIndex = 0,
+	certsIndex = 0;
 
-$(`.left1`).click(()=>{
+$(`.left1`).click( () => {
 	prodIndex = prevHref($('.item-info .item-title'), prodIndex, prods);
 });
 
-$(`.right1`).click(()=>{
+$(`.right1`).click( () => {
 	prodIndex = nextHref($('.item-info .item-title'), prodIndex, prods);
 });
 
-$(`.left3`).click(()=>{
+$(`.left3`).click( () => {
 	certsIndex = prevHref($('.item-watch .item-title'), certsIndex, certs);
 });
 
-$(`.right3`).click(()=>{
+$(`.right3`).click( () => {
 	certsIndex = nextHref($('.item-watch .item-title'), certsIndex, certs);
 });
 
@@ -65,6 +65,23 @@ const loadXML = lang => {
 const getXML = xml => {
 
 	const bodyXML = xml.responseXML.getElementsByClassName(`body`)[0];	
-	console.log(bodyXML);
 	$(`.body`).html(bodyXML.innerHTML);
 };
+
+const btnScroll = $(`.btn-scroll`);
+const topSize = 260;
+
+window.onscroll = () => scrollFunction();
+
+const scrollFunction = () => {
+	if (document.body.scrollTop > topSize || document.documentElement.scrollTop > topSize) {
+		btnScroll.css(`display`, `block`);
+	} else {
+		btnScroll.css(`display`, `none`);
+	}
+}
+
+const scrollToTop = () => {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
